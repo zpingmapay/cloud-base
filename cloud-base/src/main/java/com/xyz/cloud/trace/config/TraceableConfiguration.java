b1e14ddf9c804dc8a17ce59bda34a20f43cbd209
马再平 <mazaiping@51zhaoyou.com>
2020-07-29 11:47:33 +0800

    refactoring

--- cloud-base/src/main/java/com/xyz/cloud/trace/config/TraceableConfiguration.java
+++ cloud-base/src/main/java/com/xyz/cloud/trace/config/TraceableConfiguration.java
@@ -13,8 +13,8 @@ public class TraceableConfiguration {
     }
 
     @Bean
-    public TraceableAspect restLogAspect(@Value("${cloud.log.with-header:false}") boolean logWithHeader,
+    public TraceableAspect traceableAspect(@Value("${cloud.trace.with-header:false}") boolean withHeader,
                                          HttpHeadersHolder holder) {
-        return new TraceableAspect(logWithHeader, holder);
+        return new TraceableAspect(withHeader, holder);
     }
 }
