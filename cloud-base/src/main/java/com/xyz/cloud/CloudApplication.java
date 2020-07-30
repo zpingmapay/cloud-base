c3525cfeabb95610a0c7d80bc878abb581927575
马再平 <mazaiping@51zhaoyou.com>
2020-07-30 09:51:17 +0800

    add comments

--- cloud-base/src/main/java/com/xyz/cloud/CloudApplication.java
+++ cloud-base/src/main/java/com/xyz/cloud/CloudApplication.java
@@ -5,10 +5,21 @@ import com.xyz.cloud.jwt.annotation.EnableJwt;
 import com.xyz.cloud.lock.annotation.EnableLock;
 import com.xyz.cloud.retry.annotation.EnableRetryableEvent;
 import com.xyz.cloud.trace.annotation.EnableTraceable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
+/**
+ * Indicates a {@link Configuration configuration} class that declares one or more
+ * {@link Bean @Bean} methods and also import {@link DefaultGlobalExceptionHandler advice}.
+ * This is a convenience annotation that is equivalent to declaring {@code @EnableJwt},
+ * {@code @EnableLock}, {@code @EnableRetryableEvent}, {@code EnableRetryableEvent}
+ * and {@code EnableTraceable}.
+ *
+ * @author Zaiping Ma
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
