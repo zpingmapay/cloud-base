e91cd8b1dfd16a5824e9c999793e0f6c929b6a04
sxl <shixiulai217@163.com>
2020-07-30 15:30:22 +0800

    优化

--- sample-app/src/main/java/com/xyz/cloud/sample/client/feign/PaymentFeignClient.java
+++ sample-app/src/main/java/com/xyz/cloud/sample/client/feign/PaymentFeignClient.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @author sxl
  * @since 2020/7/23 15:40
  */
-@FeignClient(name = "payment-service", url = "http://test.pay-trade.51zhaoyou.com")
+@FeignClient(name = "remote-service-2", url = "${cloud.client.remote-service-2.url}")
 public interface PaymentFeignClient {
 
     /**
