b74e3f8bc3c215124d7c702e8e664d3b4a5e1804
马再平 <mazaiping@51zhaoyou.com>
2020-07-31 10:57:52 +0800

    retryable test

--- sample-app/src/test/java/com/xyz/cloud/sample/retry/RetryableEventTest.java
+++ sample-app/src/test/java/com/xyz/cloud/sample/retry/RetryableEventTest.java
@@ -12,13 +12,20 @@ public class RetryableEventTest {
     SampleEventPublisher eventPublisher;
 
     @Test
-    public void testEvent() throws InterruptedException {
-        SampleEvent event1 = new SampleEvent("Test1", "1 ");
+    public void testEvent1() throws InterruptedException {
+        SampleEvent event1 = new SampleEvent("Test1", "1");
+        eventPublisher.publish(event1);
+        TimeUnit.MINUTES.sleep(2);
+    }
+
+    @Test
+    public void testMultiEvent() throws InterruptedException {
+        SampleEvent event1 = new SampleEvent("Test1", "1");
         eventPublisher.publish(event1);
         SampleEvent event2 = new SampleEvent("Test2", "2");
         eventPublisher.publish(event2);
         SampleEvent event3 = new SampleEvent("Test3", "3");
         eventPublisher.publish(event3);
-        TimeUnit.MINUTES.sleep(10);
+        TimeUnit.MINUTES.sleep(2);
     }
 }
