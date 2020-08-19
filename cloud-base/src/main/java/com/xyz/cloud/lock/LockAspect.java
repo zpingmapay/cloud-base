7b110e811cd57383b206b575ed82181edb573a05
马再平 <mazaiping@51zhaoyou.com>
2020-08-19 09:41:22 +0800

    spel utils

--- cloud-base/src/main/java/com/xyz/cloud/lock/LockAspect.java
+++ cloud-base/src/main/java/com/xyz/cloud/lock/LockAspect.java
@@ -3,26 +3,18 @@ package com.xyz.cloud.lock;
 import com.xyz.cloud.lock.annotation.Lock;
 import com.xyz.cloud.lock.provider.LockProvider;
 import com.xyz.exception.ValidationException;
+import com.xyz.utils.SpelUtils;
 import com.xyz.utils.ValidationUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.context.expression.MethodBasedEvaluationContext;
-import org.springframework.core.DefaultParameterNameDiscoverer;
-import org.springframework.core.ParameterNameDiscoverer;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 import javax.validation.constraints.NotNull;
-import java.lang.reflect.Method;
 
 @Slf4j
 @Aspect
 public class LockAspect {
-    private final ParameterNameDiscoverer parameterNameDiscoverer = new DefaultParameterNameDiscoverer();
-    private static final ExpressionParser parser = new SpelExpressionParser();
     private final LockProvider lockProvider;
 
     public LockAspect(@NotNull LockProvider lockProvider) {
@@ -33,7 +25,7 @@ public class LockAspect {
     public Object lock(ProceedingJoinPoint pjp, Lock annotation) throws Throwable {
         ValidationUtils.notNull(annotation, "Lock is not properly configured");
         ValidationUtils.notBlank(annotation.key(), "Lock is not properly configured");
-        String lockKey = parseKey(annotation.key(), pjp);
+        String lockKey = SpelUtils.parse(annotation.key(), pjp);
 
         LockProvider.Lock lock = lockProvider.getLock(lockKey);
         try {
@@ -48,12 +40,5 @@ public class LockAspect {
                 lock.unlock();
             }
         }
-    }
-
-    private String parseKey(String key, ProceedingJoinPoint pjp) {
-        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
-        Method method = methodSignature.getMethod();
-        MethodBasedEvaluationContext context = new MethodBasedEvaluationContext(pjp.getTarget(), method, pjp.getArgs(), parameterNameDiscoverer);
-        return parser.parseExpression(key).getValue(context, String.class);
     }
 }
---
+++ common-utils/src/main/java/com/xyz/utils/SpelUtils.java
@@ -0,0 +1,33 @@
+package com.xyz.utils;
+
+import com.xyz.function.TryWithCatch;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.context.expression.MethodBasedEvaluationContext;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import java.lang.reflect.Method;
+
+public class SpelUtils {
+    private final static ExpressionParser parser = new SpelExpressionParser();
+
+    public static String parse(String exp, ProceedingJoinPoint pjp) {
+        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+        Method method = methodSignature.getMethod();
+        MethodBasedEvaluationContext context = new MethodBasedEvaluationContext(pjp.getTarget(), method, pjp.getArgs(), new DefaultParameterNameDiscoverer());
+
+        return TryWithCatch.apply(() -> parser.parseExpression(exp).getValue(context, String.class), method.getName());
+    }
+
+    public static <C, R> R parse(String exp, C contextObject, Class<R> clazz, R defaultResult) {
+        return TryWithCatch.apply(() -> {
+            Expression expression = parser.parseExpression(exp);
+            StandardEvaluationContext context = new StandardEvaluationContext(contextObject);
+            return expression.getValue(context, clazz);
+        }, defaultResult);
+    }
+}
