61ff089f67ed67749e9e9730f08b4770ef0053b7
sxl <shixiulai217@163.com>
2020-08-24 17:54:28 +0800

    添加注解实现枚举code校验器

---
+++ common-utils/src/main/java/com/xyz/validators/annotation/CodeOfEnum.java
@@ -0,0 +1,36 @@
+package com.xyz.validators.annotation;
+
+import com.xyz.validators.validator.CodeOfEnumValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * code of enum validator
+ *
+ * @author sxl
+ * @since 2020/8/24 15:40
+ */
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = CodeOfEnumValidator.class)
+public @interface CodeOfEnum {
+
+    Class<? extends Enum<?>> enumClass();
+
+    String codeFieldName() default "code";
+
+    String message() default "must be any of enum {enumClass}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
