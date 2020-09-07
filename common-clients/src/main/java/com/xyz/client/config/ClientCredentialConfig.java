b19173351b5b84ce3a37017383053d0795e4a7b9
sxl <shixiulai217@163.com>
2020-09-07 17:10:16 +0800

    feignclient增加雷子Sapi验签优化

--- common-clients/src/main/java/com/xyz/client/config/ClientCredentialConfig.java
+++ common-clients/src/main/java/com/xyz/client/config/ClientCredentialConfig.java
@@ -3,7 +3,6 @@ package com.xyz.client.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import javax.validation.constraints.NotBlank;
 import java.util.Comparator;
 import java.util.Map;
 
@@ -23,21 +22,15 @@ public class ClientCredentialConfig {
 
     @Data
     public static class OAuthConfig {
-        @NotBlank(message = "cloud.client.oauth.*.url is not allow empty")
         private String url;
-        @NotBlank(message = "cloud.client.oauth.*.key is not allow empty")
         private String key;
-        @NotBlank(message = "cloud.client.oauth.*.secret is not allow empty")
         private String secret;
     }
 
     @Data
     public static class SignerConfig {
-        @NotBlank(message = "cloud.client.signer.*.url is not allow empty")
         private String url;
-        @NotBlank(message = "cloud.client.signer.*.appId is not allow empty")
         private String appId;
-        @NotBlank(message = "cloud.client.signer.*.appKey is not allow empty")
         private String appKey;
     }
 
