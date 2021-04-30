802b66041e64f91121ad471680a160dc9ff3b379
马再平 <mazaiping@51zhaoyou.com>
2021-04-30 09:54:38 +0800

    test atomic long

--- sample-app/src/test/java/com/xyz/cloud/sample/utils/AtomicLongTest.java
+++ sample-app/src/test/java/com/xyz/cloud/sample/utils/AtomicLongTest.java
@@ -9,6 +9,7 @@ import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Slf4j
@@ -16,36 +17,31 @@ import javax.annotation.Resource;
 public class AtomicLongTest {
     @Resource
     private RedissonClient redissonClient;
+    private AtomicLong.CleanListener listener = (x) -> {
+        log.info("{} is cleaned", x);
+        throw new IllegalStateException("count out");
+    };
 
     @Test
-    public void should_all_pass() {
-        AtomicLong.CleanListener listener = (x) -> {
-            log.info("{} is cleaned", x);
-            throw new IllegalStateException("count out");
-        };
-        AtomicLong atomicLong = new AtomicLong("test", "key1", 0, 11, redissonClient, 1, listener);
+    public void case1() {
+        AtomicLong atomicLong = new AtomicLong("test", "key", 0, 11, redissonClient, 1, listener);
         for (int i = 0; i < 10; i++) {
             int index = (int) atomicLong.incrementAndGet();
             log.info("current value: {}", index);
         }
 
-        AtomicLong atomicLong1 = new AtomicLong("test", "key1", 0, 11, redissonClient, 1, listener);
+        AtomicLong atomicLong1 = new AtomicLong("test", "key", 0, 11, redissonClient, 1, listener);
         LockTest.assertException(x -> atomicLong1.incrementAndGet(), IllegalStateException.class);
+    }
 
-        AtomicLong atomicLong2 = new AtomicLong("test", "key1", 0, 11, redissonClient, 1, listener);
+    @Test
+    public void case2() {
+        AtomicLong atomicLong2 = new AtomicLong("test", "key", 0, 11, redissonClient, 1, listener);
         LockTest.assertNoException(x -> {
-            for (int i = 0; i < 10; i++) {
+            IntStream.range(0, 10).parallel().forEach(t -> {
                 int index = (int) atomicLong2.incrementAndGet();
                 log.info("current value: {}", index);
-            }
-        });
-
-        AtomicLong atomicLong3 = new AtomicLong("test", "key2", 0, 11, redissonClient, 1, listener);
-        LockTest.assertNoException(x -> {
-            for (int i = 0; i < 10; i++) {
-                int index = (int) atomicLong3.incrementAndGet();
-                log.info("current value: {}", index);
-            }
+            });
         });
     }
 }
