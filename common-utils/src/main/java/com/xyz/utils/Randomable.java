82298523b57cfc61a5c0e79d2ffb20385080a6b6
马再平 <mazaiping@51zhaoyou.com>
2021-06-16 14:35:25 +0800

    Randomable

--- common-utils/src/main/java/com/xyz/utils/Randomable.java
+++ common-utils/src/main/java/com/xyz/utils/Randomable.java
@@ -8,11 +8,11 @@ public interface Randomable {
         int MIN_PRIORITY = 10;
 
         @Range(min = MAX_PRIORITY, max = MIN_PRIORITY, message = "Priority:[" + MAX_PRIORITY + "," + MIN_PRIORITY + "]")
-        int getPriority();
+        Integer getPriority();
     }
 
     interface ByWeight {
         @Range(min = 1, max = 100, message = "Weight:[1,100)")
-        int getWeight();
+        Integer getWeight();
     }
 }
